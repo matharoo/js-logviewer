e1eabf5dfe8d8e5f6910a7bc7ec32408bd231e32
rmatharoo <rsmatharoo@outlook.com>
2019-09-19 19:36:42 -0400

    fixed spacings, and file reader only accept json

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ import Row from "./components/Row";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       logs: [],
       headers: [],
       filter: "",
-     };
-     
-     this.sortObj = this.sortObj.bind(this);
-     this.comparison = this.comparison.bind(this);
-     this.onChange = this.onChange.bind(this);
-     this.handleFilter = this.handleFilter.bind(this);
-     this.resetViewer = this.resetViewer.bind(this);
+    };
+
+    this.sortObj = this.sortObj.bind(this);
+    this.comparison = this.comparison.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+    this.resetViewer = this.resetViewer.bind(this);
   }
 
   componentDidMount() {
-    
+
   }
- 
+
   sortObj = (key => {
     let newarr = [...this.state.logs];
     newarr.sort(this.comparison(key));
-    this.setState({logs: newarr});
+    this.setState({ logs: newarr });
   });
 
   comparison = (key => {
@@ -39,21 +39,21 @@ class App extends Component {
 
   onChange = (event) => {
     // console.log(event);
-    if(event.target.length === 0 ){
+    if (event.target.length === 0) {
       // console.log("cancel was clicked");
       // dont do anything.
     }
-    else{
+    else {
       var file = event.target.files[0];
       var reader = new FileReader();
-      
+
       reader.onload = () => {
         // console.log(reader.result);
         this.setState({ logs: JSON.parse(reader.result) })
-        const data= JSON.parse(reader.result);
+        const data = JSON.parse(reader.result);
         // console.log("data is :"+data);
-        let headers = data.reduce(function(arr, o) {
-          return Object.keys(o).reduce(function(a, k) {
+        let headers = data.reduce(function (arr, o) {
+          return Object.keys(o).reduce(function (a, k) {
             if (a.indexOf(k) === -1) a.push(k);
             return a;
           }, arr)
@@ -67,60 +67,60 @@ class App extends Component {
   }
 
   handleFilter = (e) => {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  resetViewer = (e) =>{
+  resetViewer = (e) => {
     // console.log("resetting viewer");
-    this.setState({logs: [],headers:[]});
+    this.setState({ logs: [], headers: [] });
   }
 
   render() {
-    const {filter, logs}= this.state;
+    const { filter, logs } = this.state;
     //return the matching object from logs based on any value typed in filter input, if filter is empty gives back all the logs
-    const filteredData = logs.filter(log=>{
+    const filteredData = logs.filter(log => {
       return Object.keys(log).some((key) => {
         return log[key].toLowerCase().includes(filter.toLowerCase())
       });
     })
     return (
       <div className="App">
-      <header className="App-header">
-      <div className="text-center"><h2>JS Log Viewer</h2></div>
-      <div className="container">
-        <div className="row form-group">
-          <div className="col-md-4"></div>
-          {logs.length===0 ? 
-          <div className="col-md-4 custom-file">
-            <input type="file" onChange={this.onChange} className="custom-file-input" id="customFile" name="file"/>
-            <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+        <header className="App-header">
+          <div className="text-center"><h2>JS Log Viewer</h2></div>
+          <div className="container">
+            <div className="row form-group">
+              <div className="col-md-4"></div>
+              {logs.length === 0 ?
+                <div className="col-md-4 custom-file">
+                  <input type="file" accept=".json" onChange={this.onChange} className="custom-file-input" id="customFile" name="file" />
+                  <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+                </div>
+                : <button className="btn btn-primary" type="button" onClick={this.resetViewer.bind(this)}>Import a new file</button>}
+              <div className="col-md-4">
+                {logs.length > 1 ?
+                  <input className="form-control" value={this.state.filter} onChange={this.handleFilter} placeholder="Search or filter records" name="filter" />
+                  : null}
+
+              </div>
+            </div>
           </div>
-          :<button className="btn btn-primary" type="button" onClick={this.resetViewer.bind(this)}>Import a new file</button>}
-          <div className="col-md-4">
-          {logs.length>1 ? 
-          <input className="form-control" value={this.state.filter} onChange={this.handleFilter} placeholder="Search or filter records" name="filter"/>
-          : null }
-          
-        </div>
-        </div>
+          <div className="container">
+            <div className="row">
+              <table className="table table-hover table-dark">
+                <thead>
+                  <tr>
+                    {this.state.headers.length > 0 ? this.state.headers.map((header) => <th key={header} scope="col" onClick={this.sortObj.bind(this, header)}><u>{header}</u></th>) : null}
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* checking for headers otherwise the component doesnt recieve header prop */}
+                  {filteredData.length > 0 && this.state.headers.length ? filteredData.map((rowData, index) => <Row key={index} {...rowData} header={this.state.headers} />) : null}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </header>
       </div>
-        <div className="container">
-          <div className="row">
-            <table className="table table-hover table-dark">
-              <thead>
-                <tr>
-                {this.state.headers.length>0 ? this.state.headers.map( (header) => <th key={header} scope="col" onClick={this.sortObj.bind(this, header)}><u>{header}</u></th>) : null }
-                </tr>
-              </thead>
-              <tbody>
-              {/* checking for headers otherwise the component doesnt recieve header prop */}
-                {filteredData.length>0 && this.state.headers.length ? filteredData.map( (rowData,index) => <Row key={index} {...rowData} header={this.state.headers} />): null }
-              </tbody>
-            </table>
-          </div>
-          </div>
-      </header>
-    </div>
     );
   }
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,45 +1,42 @@
 import React, { Component } from 'react';
-//if header is equal to props.[header]
-//if obj key matches with one in header then we show it up otherwise leave it blank byt setting its value to blank
-// for each row go over headers for 0 to headers.length then compare the header if its present in the obj keys if its then put the value otherwise leave it blank
 
 class Row extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       headers: [],
       color: ""
-     };
+    };
   }
 
   componentDidMount() {
     // console.log("props "+typeof this.props.header);
-    if(this.props.type!==undefined){
-      this.setState({color: this.props.type.toLowerCase()})
+    if (this.props.type !== undefined) {
+      this.setState({ color: this.props.type.toLowerCase() })
     }
-    this.setState({headers: this.props.header})
+    this.setState({ headers: this.props.header })
   }
 
   componentDidUpdate(prevProps, prevState) {
     // console.log("comoponent updated!!");
     // console.log("new type is "+this.props.type); 
     if (prevProps.type !== this.props.type) {
-      this.setState({color:this.props.type.toLowerCase()})
+      this.setState({ color: this.props.type.toLowerCase() })
     }
   }
 
   render() {
     return (
-      <tr className={this.state.color+" "+this.props.datetime}>
-      {this.state.headers.map((val,ind)=>{
-          if(this.props.hasOwnProperty(val)){
+      <tr className={this.state.color + " " + this.props.datetime}>
+        {this.state.headers.map((val, ind) => {
+          if (this.props.hasOwnProperty(val)) {
             return <td key={val}>{this.props[val]}</td>
           }
-          else{
+          else {
             return <td key={val}><pre className="text-white">---------</pre></td>
           }
         })
-      }
+        }
       </tr>
     );
   }
